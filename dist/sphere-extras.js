0eb648a6a76bc8155ac598d414a378f2f42b6861
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2015-04-15 00:08:07 +0200

    Added: * auto-save of project key, credentials and scopes in local storage * project key is propagated to scope

---
+++ dist/sphere-extras.js
@@ -0,0 +1,60 @@
+jQuery(function ($) {
+  var watchedFields = ["projectKey", "clientId", "clientSecret"];
+  var prefix = "credentials-";
+
+  $('.raml-console-sidebar-oauth-scopes input').livequery(function() {
+    $(this).change(function() {
+      $('.raml-console-sidebar-oauth-scopes input').each(function (idx, cb) {
+        localStorage.setItem(prefix + "sphere-scopes-" + idx, $(cb).is(':checked'))
+      })
+    });
+  });
+
+  $('input[name=projectKey]').livequery(function() {
+    var $scope = angular.element($(".raml-console-sidebar-oauth-scopes").get()).scope();
+
+    var rememberFields = function (prefix, fields) {
+      fields.forEach(function (f) {
+        $('input[name=' + f + ']').on('input', function() {
+          localStorage.setItem(prefix + f, $(this).val())
+        });
+      })
+    };
+
+    var restoreFields = function (prefix, fields) {
+      fields.forEach(function (f) {
+        var input = $('input[name=' + f + ']')
+        input.val(localStorage.getItem(prefix + f))
+        input.trigger('input')
+      })
+    };
+
+    var setupScopeUpdater = function (prefix, field) {
+      $('input[name=' + field + ']').on('input', function() {
+        var val = $(this).val();
+
+        $scope.$apply(function() {
+          for (var i = 0; i < $scope.scopes.length; i++) {
+            $scope.scopes[i] = $scope.scopes[i].replace(/:.*$/, ":" + val)
+            $scope.scopes[i] = $scope.scopes[i].replace(/:$/, ":{projectKey}")
+          }
+        });
+      });
+    };
+
+    var restoreScopes = function () {
+      $('.raml-console-sidebar-oauth-scopes input').each(function (idx, cb) {
+        var checked = localStorage.getItem(prefix + "sphere-scopes-" + idx) == 'true'
+
+        if (checked) {
+          $(cb).trigger('click') // robot clicking checkboxes :)
+        }
+      })
+    };
+
+    setupScopeUpdater(prefix, watchedFields[0])
+    restoreFields(prefix, watchedFields)
+    rememberFields(prefix, watchedFields)
+    restoreScopes();
+  });
+}(jQuery));
