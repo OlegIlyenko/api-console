277c0672fd2db4040ae60461af5c4579b553a2b4
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2015-06-09 17:09:40 +0200

    using inlined schemas

--- dist/scripts/api-console.js
+++ dist/scripts/api-console.js
@@ -327,7 +327,7 @@
 
               var schema = code ? $scope.responseInfo[code][$scope.responseInfo[code].currentType].schema : $scope.methodInfo.body[$scope.currentBodySelected].schema
 
-              docson.doc($docson, schema, undefined, "/sphere/schemas/");
+              docson.doc($docson, schema, undefined, "/sphere/inlined/");
               //$docson.velocity('slideDown');
             })
           } else {
